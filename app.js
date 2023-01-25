ebff5e9b67ebe8641c3082a9abe73f13689a01fa
David Colon <davidcolon07@gmail.com>
2023-01-25 09:51:17 -0800

    express session part 1

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+// commenting out cookieParser as it is incompatible with Express Session
+// var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session')
+// notice FileStore has two sets of arguments
+// require() returns another function and THAT function uses (session) as its args
+const FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -33,11 +38,32 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+
+// we are commenting out cookierParser since it is incompatible w/ Express Session
+// app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  // values are arbitrary
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  // false means any session that includes zero changes, is not saved; also, no new cookie is sent to the client
+  // this is dealing with NEW sessions THAT HAVE NOT ADDED ANY data
+  saveUninitialized: false,
+  // false means that once a session is created, updated, and saved, it will NOT continue to be resaved
+  // this is dealing with PREVIOUSLY CREATED sessions which ALREADY HAVE some data
+  resave: false,
+  // save to server's HDD, not just the app's memory
+  store: new FileStore()
+}));
 
 function auth(req, res, next) {
-  if (!req.signedCookies.user) {
+  // the 'session' middleware automatically creates a 'session' property to the request message; let's see what it says
+  console.log(req.session);
+
+  if (!req.session.user) {
+
     const authHeader = req.headers.authorization;
+
     if (!authHeader) {
       const err = new Error('You are not authenticated!');
       res.setHeader('WWW-Authenticate', 'Basic');
@@ -48,13 +74,19 @@ function auth(req, res, next) {
     const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
     const user = auth[0];
     const pass = auth[1];
+
     if (user === 'admin' && pass === 'password') {
+
+      // the below lines are commented out since cookies are being handled by Express Session
       // res.cookie is a built-in method of Express's Response object
       // res.cookie(name, value, [options]) is the template
       // name, value are essentially key:value pair
       // [options] arg is optional but tells express to use cookieParser to create a signed cookie
       // we can also include things like cookie expiration time in the third arg
-      res.cookie('user', 'admin', { signed: true })
+      // res.cookie('user', 'admin', { signed: true })
+
+      // we write to our session that 'user' = 'admin'
+      req.session.user = 'admin';
       return next(); // authorized
     } else {
       const err = new Error('You are not authenticated!');
@@ -62,12 +94,13 @@ function auth(req, res, next) {
       err.status = 401;
       return next(err);
     }
+
   } else {
-    if (req.signedCookies.user === 'admin') {
+
+    if (req.session.user === 'admin') {
       return next();
     } else {
       const err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
       err.status = 401;
       return next(err);
     }
