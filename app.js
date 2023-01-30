18d4804be16fad3fc1e0a4594378467b2d927b49
David Colon <davidcolon07@gmail.com>
2023-01-30 13:22:15 -0800

    HTTPS

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 
 var app = express();
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+    console.log(`Redirecting to https://${req.hostname}:${app.get('secPort')}${req.url}`);
+    res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`)
+  }
+})
 
 const mongoose = require('mongoose');
 
