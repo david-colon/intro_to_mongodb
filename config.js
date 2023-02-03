1c092c3e75ed93cbc26778ffee13af53238b99ef
David Colon <davidcolon07@gmail.com>
2023-02-03 11:59:17 -0800

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,10 @@
+const { FACEBOOK_APP_ID, FACEBOOK_APP_SECRET } = require('./public/constants');
+
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: FACEBOOK_APP_ID,
+        clientSecret: FACEBOOK_APP_SECRET
+    }
 }
